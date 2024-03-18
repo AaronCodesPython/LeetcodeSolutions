2c8c9ad1de4eb0ca76df52b0106c3ffbde6713da
Aaron Glaser <42847390+AaronCodesPython@users.noreply.github.com>
2024-03-18 21:52:28 +0100

    n-queens-2 working

--- n-queens-2.py
+++ n-queens-2.py
@@ -1,25 +1,59 @@
 
-
+n = 0
 def totalNQueens(n: int) -> int:
-    def changeGrid(grid,row,column):
+    def changeGrid(grid,row,col):
         for i in range(n):
             grid[row][i] = 1
-            grid[i][row] = 1
+            grid[i][col] = 1
+            
+            a = row+i < n
+            b = col+i < n
+            c = row-i >= 0
+            d = col-i >= 0
+            if a and b:  
+                grid[row+i][col+i] = 1
+            if c and d:
+                grid[row-i][col-i] = 1
+            if a and d:
+                grid[row+i][col-i] = 1
+            if b and c:
+                grid[row-i][col+i] = 1  
             #grid[row][row]
         return grid
         # O(n)
     def dfs(np,cr,grid):
         if np == n:
-            print("Placed all")
+            nonlocal n
+            n+=1
             return
         for pos in range(n):
-            if grid[cr][n] != 1:
-                grid[cr][n] = 1
-                dfs(np+1,cr+1, grid)
+            if grid[cr][pos] != 1:
+                newGrid = [row[:] for row in grid]  
+                newGrid[cr][pos] = 1
+                newGrid = changeGrid(newGrid, cr, pos).copy()
+                dfs(np+1,cr+1, newGrid)
             
     grid = [[0]*n for _ in range(n)]
-    changeGrid(grid, 3,4)
-    print(grid)
-    return 0
+    
 
-totalNQueens(8)
+    dfs(0,0,grid)
+    return n
+
+totalNQueens(6)
+
+'''
+row-i col-i >= 0
+row+i col+i
+row-i col+1
+row+i col-1  
+[0, 1, 0, 0, 1, 0, 0, 1], 
+[0, 0, 1, 0, 1, 0, 1, 0], 
+[0, 0, 0, 1, 1, 1, 0, 0], 
+[1, 1, 1, 1, 1, 1, 1, 1], 
+[0, 0, 0, 1, 1, 1, 0, 0], 
+[0, 0, 1, 0, 1, 0, 1, 0], 
+[0, 1, 0, 0, 1, 0, 0, 1], 
+[1, 0, 0, 0, 1, 0, 0, 0]]
+4 4
+
+'''
