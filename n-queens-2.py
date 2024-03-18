111c0f2fcaafef1c6843efda2c0149ecf1049d20
Aaron Glaser <42847390+AaronCodesPython@users.noreply.github.com>
2024-03-18 22:20:17 +0100

    added optimized solution

--- n-queens-2.py
+++ n-queens-2.py
@@ -1,7 +1,8 @@
 
-n = 0
+
 def totalNQueens(n: int) -> int:
     def changeGrid(grid,row,col):
+        
         for i in range(n):
             grid[row][i] = 1
             grid[i][col] = 1
@@ -21,10 +22,11 @@ def totalNQueens(n: int) -> int:
             #grid[row][row]
         return grid
         # O(n)
+    res = 0
     def dfs(np,cr,grid):
         if np == n:
-            nonlocal n
-            n+=1
+            nonlocal res
+            res+=1
             return
         for pos in range(n):
             if grid[cr][pos] != 1:
@@ -37,9 +39,7 @@ def totalNQueens(n: int) -> int:
     
 
     dfs(0,0,grid)
-    return n
-
-totalNQueens(6)
+    return res
 
 '''
 row-i col-i >= 0
@@ -57,3 +57,36 @@ row+i col-1
 4 4
 
 '''
+
+# better solution:
+def totalNQueens_optimized(n):
+    col = set()
+    posDiag = set()
+    negDiag = set()
+
+    res = 0
+    def backtrack(r): # r = row
+        if r == n:
+            nonlocal res
+            res+=1
+            return
+        for c in range(n):
+            if c in col or (r+c) in posDiag or (r-c) in negDiag:
+                continue
+            col.add(c)
+            posDiag.add(r+c)
+            negDiag.add(r-c)
+            backtrack(r+1)
+
+            #backtrack
+            col.remove(c)
+            posDiag.remove(r+c)
+            negDiag.remove(r-c)
+    backtrack(0)
+    return res
+
+#print(totalNQueens(12)) -> 4.19 sec
+#print(totalNQueens_optimized(12)) -> 1.254 sec
+#print(totalNQueens(13)) -> 23.549 seconds
+#print(totalNQueens_optimized(13)) -> 6.645 seconds
+print(totalNQueens_optimized(14))
